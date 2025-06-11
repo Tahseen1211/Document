5ad01a7acec959671aa678cbea702e10253c355e
Tahseen sultana <tahseenah401@gmail.com>
2025-06-11 17:49:11 +0530

    first commit

---
+++ marks.sql
@@ -0,0 +1,21 @@
+mysql> use marks;
+Database changed
+mysql> show tables;
++-----------------+
+| Tables_in_marks |
++-----------------+
+| marks           |
++-----------------+
+1 row in set (0.00 sec)
+
+mysql> select * from marks;
++------+--------+-------+---------+
+| S_no | Telugu | hindi | english |
++------+--------+-------+---------+
+|    1 |     99 |    98 |      99 |
+|    2 |     68 |    89 |      97 |
+|    3 |     78 |    98 |      87 |
+|    4 |     67 |    87 |      96 |
+|    5 |     25 |    45 |      99 |
++------+--------+-------+---------+
+5 rows in set (0.01 sec)
